78930c58599c1bcac2f8a848f6e92edb45ee9baa
root <root@localhost.localdomain>
2023-02-13 18:18:00 +0100

    Add function search book

--- web/code/views/api.py
+++ web/code/views/api.py
@@ -1,3 +1,4 @@
+import json
 import os
 import sys
 
@@ -17,4 +18,21 @@ def insertar_ingrediente():
 
 
 def buscar_recetas_por_ingredientes():
-   pass
+   engine = get_db_engine()
+   ingredientes = json.dumps(request.json)
+   incluye = ["sum(id_ingrediente = {}) > 0".format(number) for number in (request.json['incluye'] if 'incluye' in request.json else [])]
+   excluye = ["sum(id_ingrediente = {}) = 0".format(number) for number in (request.json['excluye'] if 'excluye' in request.json else [])]
+   condiciones = incluye + excluye
+
+   sql = """
+   select recetas.nombre FROM (
+	   select id_receta, ({condiciones}) as c2
+      from ingredientes_receta group by id_receta
+   ) t1 INNER JOIN recetas ON recetas.id = t1.id_receta
+   WHERE c2 = 1
+   """.format(condiciones = " AND ".join(condiciones) if len(condiciones) > 0 else "1")
+
+   recetas = engine.execute(sql).fetchall()
+   recetas = [row['nombre'] for row in recetas]
+
+   return recetas
