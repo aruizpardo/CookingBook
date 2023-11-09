e3bb13bef463f9c59b9f73db0dab510bacd827da
ALEJANDRO RUIZ PARDO <alejandro.ruiz.pardo@altia.es>
2023-11-09 13:58:54 +0100

    Create basic database

--- data/init.sql
+++ data/init.sql
@@ -1,45 +1,54 @@
-drop table if exists ingredientes;
-create table ingredientes (
-    id INTEGER PRIMARY KEY,
-    nombre varchar(255) NOT NULL UNIQUE
+-- Crear una tabla de tipo de recetas
+DROP TABLE IF EXISTS Tipo
+CREATE TABLE Tipo (
+    TipoID INTEGER PRIMARY KEY,
+    Nombre TEXT NOT NULL,
 );
 
-drop table if exists tipos;
-create table tipos (
-    id INTEGER PRIMARY KEY,
-    nombre varchar(255) NOT NULL
+-- Crear una tabla para donde cocinar la receta
+DROP TABLE IF EXISTS LugaresCocina
+CREATE TABLE LugaresCocina (
+    LugarCocinaID INTEGER PRIMARY KEY,
+    Nombre TEXT NOT NULL
 );
 
-drop table if exists recetas;
-create table recetas (
-    id INTEGER PRIMARY KEY,
-    nombre varchar(255) NOT NULL,
-    id_tipo int NULL,
-    tiempo_minutos int NOT NULL,
-    FOREIGN KEY (id_tipo) REFERENCES tipos(id)
+-- Crear una tabla para las recetas
+DROP TABLE IF EXISTS Recetas
+CREATE TABLE Recetas (
+    RecetaID INTEGER PRIMARY KEY,
+    Nombre TEXT NOT NULL,
+    Descripcion TEXT,
+    TiempoPreparacion INTEGER,
+    TipoID INTEGER,
+    Porciones INTEGER,
+    LugarCocinaID INTEGER,
+    FOREIGN KEY (LugarCocinaID) REFERENCES LugaresCocina(LugarCocinaID),
+    FOREIGN KEY (TipoID) REFERENCES Tipo(TipoID)
 );
 
-drop table if exists ingredientes_receta;
-create table ingredientes_receta (
-    id_receta int NOT NULL,
-    id_ingrediente int NOT NULL,
-    cantidad string NOT NULL,
-    FOREIGN KEY (id_receta) REFERENCES recetas(id),
-    FOREIGN KEY (id_ingrediente) REFERENCES ingredientes(id)
+-- Crear una tabla para los ingredientes
+DROP TABLE IF EXISTS Ingredientes
+CREATE TABLE Ingredientes (
+    IngredienteID INTEGER PRIMARY KEY,
+    Nombre TEXT NOT NULL
 );
 
-drop table if exists instrucciones;
-create table instrucciones (
-    id INTEGER PRIMARY KEY,
-    instruccion varchar(255) NOT NULL,
-    id_receta int NOT NULL,
-    FOREIGN KEY (id_receta) REFERENCES recetas(id)
+-- Crear una tabla para la relación entre recetas e ingredientes
+DROP TABLE IF EXISTS RecetaIngredientes
+CREATE TABLE RecetaIngredientes (
+    RecetaID INTEGER,
+    IngredienteID INTEGER,
+    Cantidad TEXT,
+    FOREIGN KEY (RecetaID) REFERENCES Recetas(RecetaID),
+    FOREIGN KEY (IngredienteID) REFERENCES Ingredientes(IngredienteID)
 );
 
-drop table if exists orden_instrucciones;
-create table orden_instrucciones (
-    id INTEGER PRIMARY KEY,
-    intruccion_anterior int NULL,
-    FOREIGN KEY (id) REFERENCES instrucciones(id),
-    FOREIGN KEY (intruccion_anterior) REFERENCES instrucciones(id)
+-- Crear una tabla para las instrucciones de preparación
+DROP TABLE IF EXISTS Instrucciones
+CREATE TABLE Instrucciones (
+    InstruccionID INTEGER PRIMARY KEY,
+    RecetaID INTEGER,
+    Paso INTEGER,
+    Descripcion TEXT,
+    FOREIGN KEY (RecetaID) REFERENCES Recetas(RecetaID)
 );
